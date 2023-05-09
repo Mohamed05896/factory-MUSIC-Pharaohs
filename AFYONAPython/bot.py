21ac98c43fb2cfe86e31a9c4b946a41b85f69fff
Mohamed05896 <125174475+Mohamed05896@users.noreply.github.com>
2023-05-09 19:20:35 +0300

    Add files via upload

---
+++ AFYONAPython/bot.py
@@ -0,0 +1,23 @@
+from pyrogram import Client, idle
+#'‹ ٰ💸 ⇣ سورس الفراعنة ⇣ 💸 › .'#
+from pyromod import listen
+
+
+
+bot = Client(
+    "mo",
+    api_id=23556891,
+    api_hash="ef55a96d104cfd2131d81bb3e8467a13",
+    bot_token="5637886104:AAGMbbnYU7JZTgPvR_wwfGRMrTKiwiyTqGE",#توكن المصنع
+    plugins=dict(root="MHelal")
+    )
+
+async def start_helalbot():
+    print("تم تشغيل الصانع بنجاح..💗")
+    await bot.start()
+    hh = "@DEV_Abdullah"#يوزر المطور المصنع
+    try:
+        await bot.send_message(hh, "**تم تشغيل الصانع بنجاح ...🥀**")
+    except:
+        pass
+    await idle()
